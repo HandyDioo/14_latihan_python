7d014e30b7dcfdbdc5a12481fea883fa3f7d7688
Cihuyy <handydio07@gmail.com>
2026-08-03 14:33:57 +0700

    Add files via upload

---
+++ ganjilgenap.py
@@ -0,0 +1,8 @@
+#Menentukan Bilangan Ganjil atau Genap
+
+angka = int(input("Masukkan sebuah bilangan : "))
+
+if angka % 2 == 0:
+    print(angka, "adalah bilangan GENAP")
+else:
+    print(angka, "adalah bilangan GANJIL")
