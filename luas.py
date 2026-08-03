0b3ec9dcd8237657941e42bde0c67f022ec0fd69
Cihuyy <handydio07@gmail.com>
2026-08-03 14:07:23 +0700

    Add files via upload

---
+++ luas.py
@@ -0,0 +1,8 @@
+print("Menghitung Luas Persegi Panjang")
+
+p = float(input("Masukkan panjang : "))
+l = float(input("Masukkan lebar : "))
+
+luas = p * l
+
+print("Luas persegi panjang =", luas, "cm")
