dd1dd46ad9080c64fb372add1581a0018a97be1c
HandyDioo <handydio07@gmail.com>
2026-08-30 14:32:01 +0700

    Menambahkan Menu pada Program Python

--- ganjilgenap.py
+++ ganjilgenap.py
@@ -6,24 +6,50 @@ def cek_ganjil_genap(angka):
 
 def cek_prima(angka):
     if angka < 2:
-        print(angka, "bukan bilangan prima")
+        print(angka, "BUKAN bilangan prima")
         return
 
     for i in range(2, angka):
         if angka % i == 0:
-            print(angka, "bukan bilangan prima")
+            print(angka, "BUKAN bilangan prima")
             return
-
-    print(angka, "adalah bilangan prima")
-
+        
+    print(angka, "ADALAH bilangan prima")
 
 while True:
-    angka = int(input("Masukkan sebuah bilangan : "))
+    print("\n=== MENU PROGRAM PYTHON ===")
+    print("1. Program Ganjil Genap")
+    print("2. Program Bilangan Prima")
+    print("3. Exit")
 
-    cek_ganjil_genap(angka)
-    cek_prima(angka)
-    ulang = input("Apakah Anda ingin memasukkan bilangan lain? (Y/N): ")
+    pilihan = input("Pilih Menu: ")
 
-    if ulang.upper() != "Y":
+    if pilihan == "1":
+
+        while True:
+            angka = int(input("Masukkan sebuah bilangan: "))
+            cek_ganjil_genap(angka)
+
+            ulang = input("Ingin memasukkan bilangan lagi? (Y/N): ")
+
+            if ulang.upper() != "Y":
+                break
+
+    elif pilihan == "2":
+
+        while True:
+            angka = int(input("Masukkan sebuah bilangan: "))
+            cek_prima(angka)
+
+            ulang = input("Ingin memasukkan bilangan prima lagi? (Y/N): ")
+
+            if ulang.upper() != "Y":
+                break
+
+    elif pilihan == "3":
+        print("Keluar program...")
         print("Program selesai. Have a nice day!")
         break
+
+    else:
+        print("Pilihan tidak valid.")
