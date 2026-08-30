e78fa63f74c8c8382592de2a743e69870947f383
HandyDioo <handydio07@gmail.com>
2026-08-30 14:13:54 +0700

    Menambahkan Fungsi pada Bilangan Prima

--- ganjilgenap.py
+++ ganjilgenap.py
@@ -4,14 +4,26 @@ def cek_ganjil_genap(angka):
     else:
         print(angka, "adalah bilangan GANJIL")
 
+def cek_prima(angka):
+    if angka < 2:
+        print(angka, "bukan bilangan prima")
+        return
+
+    for i in range(2, angka):
+        if angka % i == 0:
+            print(angka, "bukan bilangan prima")
+            return
+
+    print(angka, "adalah bilangan prima")
+
 
 while True:
     angka = int(input("Masukkan sebuah bilangan : "))
 
     cek_ganjil_genap(angka)
-
+    cek_prima(angka)
     ulang = input("Apakah Anda ingin memasukkan bilangan lain? (Y/N): ")
 
     if ulang.upper() != "Y":
-        print("Program selesai.")
+        print("Program selesai. Have a nice day!")
         break
